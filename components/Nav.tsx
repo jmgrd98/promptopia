dd90335cd6c74c1b80b873a1197a83987d750dd8
João Marcelo Dantas <jmgrd98@gmail.com>
2023-07-23 17:15:16 -0300

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,12 +6,10 @@ import { useState, useEffect } from 'react'
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 export default function Nav() {
-    const isUserLogged = false
-
-
-    // const { data: session } = useSession();
+    const isUserLogged = true
 
     const [providers, setProviders] = useState(null)
+    const [toggleDropdown, setToggleDropdown] = useState(false)
 
     useEffect(() => {
         const setProviders = async () => {
@@ -37,6 +35,54 @@ export default function Nav() {
                     <Link href='/profile'>
                         <Image src='/assets/images/logo.svg' width={37} height={37} alt='Profile image' className='rounded-full'/>
                     </Link>
+                </div>
+            ) : (
+            <>
+            {providers && Object.values(providers).map((provider: any) => (
+                <button type='button' key={provider.name} onClick={() => signIn(provider.id)} className='black_btn'>Sign In</button>
+            ))}
+            </>
+            )}
+        </div>
+
+        <div className='sm:hidden flex relative'>
+            {isUserLogged ? (
+                <div className='flex gap-3 md:gap-5'>
+                    <Image
+                    src='/assets/images/logo.svg'
+                    width={37}
+                    height={37}
+                    alt='Profile image'
+                    className='rounded-full'
+                    onClick={() => setToggleDropdown((prevState) => (!prevState))}
+                     />
+
+                     {toggleDropdown && (
+                        <div className='dropdown'>
+                            <Link
+                            href='/profile'
+                            className='dropdown_link'
+                            onClick={() => setToggleDropdown(false)}
+                            >
+                            My Profile
+                            </Link>
+                            <Link
+                            href='/create-prompt'
+                            className='dropdown_link'
+                            onClick={() => setToggleDropdown(false)}
+                            >
+                            Create Prompt
+                            </Link>
+                            <button type='button' onClick={() => {
+                                setToggleDropdown(false)
+                                signOut()
+                            }}
+                            className='mt-5 w-full black_btn'
+                            >
+                            Sign Out
+                            </button>
+                        </div>
+                        )}
                 </div>
             ) : (
             <>
