e6b6619fcff61d39f0d339c067ce071f787d8229
João Marcelo Dantas <jmgrd98@gmail.com>
2023-07-24 17:36:56 -0300

    Update Form.tsx

--- components/Form.tsx
+++ components/Form.tsx
@@ -32,7 +32,6 @@ export default function Form({type, post, setPost, submitting, handleSubmit}: an
               Tag {` `}
               <span className="font-normal">(#product, #webdevelopment, #idea)</span>
               </span>
-          </label>
 
           <input
           value={post.tag}
@@ -40,10 +39,20 @@ export default function Form({type, post, setPost, submitting, handleSubmit}: an
           placeholder="#tag"
           className="form_input"
           required
-          >
+          />
 
-          </input>
+          </label>
 
+          <div className="flex-end mx-3 mb-5 gap-4">
+            <Link href="/" className="text-gray-500 text-sm">Cancel</Link>
+
+            <button
+            type="submit"
+            disabled={submitting}
+            className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white">
+              {submitting ? `${type}...` : type}
+            </button>
+          </div>
         </form>
     </section>
   )
