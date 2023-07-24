4e22d96d35c72f74bb625a726a2bffa859b79eff
João Marcelo Dantas <jmgrd98@gmail.com>
2023-07-23 23:07:57 -0300

    CreatePrompt component

---
+++ app/create-prompt/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import {useState} from 'react'
+import {useSession} from 'next-auth/react'
+import {useRouter} from 'next/navigation'
+import Form from '@components/Form'
+
+export default function CreatePrompt() {
+
+    const [submitting, setSubmitting] = useState(false)
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: '',
+    });
+
+    const createPrompt = async (e: any) => {
+        e.preventDefault()
+        setSubmitting(true)
+    }
+
+  return (
+    <Form
+    type="Create"
+    post={post}
+    setPost={setPost}
+    submitting={submitting}
+    handleSubmit={createPrompt}
+    />
+  )
+}
--- components/Form.tsx
+++ components/Form.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import Link from "next/link"
 
-export default function Form() {
+export default function Form({type, post, setPost, submitting, handleSubmit}: any) {
   return (
-    <div>Form</div>
+    <section className="w-full max-w-full flex-start flex-col">
+        <h1 className="head_text text-left">{type} Post</h1>
+    </section>
   )
 }
