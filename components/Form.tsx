facc1471373f9208f58fedb512e0baddf52a4dbc
João Marcelo Dantas <jmgrd98@gmail.com>
2023-07-24 16:31:34 -0300

    Update Form.tsx

--- components/Form.tsx
+++ components/Form.tsx
@@ -8,7 +8,43 @@ export default function Form({type, post, setPost, submitting, handleSubmit}: an
           </h1>
 
         <p className="desc text-left max-w-md">{type} and share amazing prompts with the world, and let your imagination run wild with any AI-powered platform.</p>
+        <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+        >
 
+          <label>
+            <span className="font-satoshi font-semibold text-base text-gray-700">Your AI Prompt</span>
+          </label>
+
+          <textarea
+          value={post.prompt}
+          onChange={(e) => setPost({...post, prompt: e.target.value})}
+          placeholder="Write your prompt here..."
+          className="form_textarea"
+          required
+          >
+
+          </textarea>
+
+          <label>
+            <span className="font-satoshi font-semibold text-base text-gray-700">
+              Tag {` `}
+              <span className="font-normal">(#product, #webdevelopment, #idea)</span>
+              </span>
+          </label>
+
+          <input
+          value={post.tag}
+          onChange={(e) => setPost({...post, tag: e.target.value})}
+          placeholder="#tag"
+          className="form_input"
+          required
+          >
+
+          </input>
+
+        </form>
     </section>
   )
 }
