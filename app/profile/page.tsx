e47c2d09e6833255ac2d2c0ac1b7f490b4155016
João Marcelo Dantas <jmgrd98@gmail.com>
2023-07-25 09:16:37 -0300

    Create page.tsx

---
+++ app/profile/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import {useState, useEffect} from 'react'
+import { useSession } from 'next-auth/react'
+import {useRouter} from 'next/navigation'
+
+import Profile from '@components/Profile'
+
+export default function page() {
+  return (
+    <div>page</div>
+  )
+}
