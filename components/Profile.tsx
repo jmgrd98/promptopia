6d0d32391f4dbb3a2cf851300ab160357de202a0
João Marcelo Dantas <jmgrd98@gmail.com>
2023-07-26 16:24:47 -0300

    Update Profile.tsx

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -3,7 +3,7 @@ import PromptCard from "./PromptCard"
 export default function Profile({name, desc, data, handleEdit, handleDelete}: any) {
   return (
     <section className="w-full">
-      <h1>{name} Profile</h1>
+      <h1 className="head_text text-left">{name} Profile</h1>
     </section>
   )
 }
