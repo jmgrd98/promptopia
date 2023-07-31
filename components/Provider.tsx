0c2d64e5cbcdc01a3d300d4e2eedd447849708c9
João Marcelo Dantas <jmgrd98@gmail.com>
2023-07-31 11:38:43 -0300

    commit

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -2,9 +2,9 @@
 import { SessionProvider } from 'next-auth/react'
 
 export default function Provider({ children, session }: any) {
-  return (
-    <SessionProvider session={session}>
-        {children}
-    </SessionProvider>
-  )
+    return (
+        <SessionProvider session={session}>
+            {children}
+        </SessionProvider>
+    )
 }
