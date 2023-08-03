47d86098af41759ff07b1045ddb6952ce85ed2fd
João Marcelo Dantas <jmgrd98@gmail.com>
2023-08-03 18:40:42 -0300

    Update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -19,7 +19,7 @@ export default function MyProfile() {
             setPosts(data)
         }
 
-        if (session?.user.id) fetchPosts()
+        if (session.user.id) fetchPosts()
     }, []);
 
     const handleEdit = (post) => {
