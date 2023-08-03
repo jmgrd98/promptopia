5ac2e000ba6c0d462961cbc7ef8ece06834ac90a
João Marcelo Dantas <jmgrd98@gmail.com>
2023-08-03 18:29:23 -0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ import User from "@models/User";
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID.toString(),
+            clientId: String(process.env.GOOGLE_CLIENT_ID),
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
